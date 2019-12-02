aadaf98eb5bb6445818591ad3f83befaf85f644f
Weston Carpenter <wc.cc96@gmail.com>
2019-12-02 15:14:32 -0800

    Added products overview screen with rendered seed data

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -2,7 +2,7 @@ import PRODUCTS from '../../data/seed'
 
 const initialState = {
     availableProducts: PRODUCTS,
-    userProducts: PRODUCTS.filter(prod.ownerId === 'u1')
+    userProducts: PRODUCTS.filter(prod => prod.ownerId === 'u1')
 };
 
 export default (state = initialState, action) => {
