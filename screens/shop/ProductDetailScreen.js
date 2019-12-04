52e9fcccebd238337c04ad68a6dc098b8584cae0
Weston Carpenter <wc.cc96@gmail.com>
2019-12-03 19:59:27 -0800

    selected product info sent to detail screen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { View, StyleSheet, Text, Image, Button, ScrollView } from 'react-native';
+import { useSelector } from 'react-redux';
 
 const ProductDetailScreen = props => {
 
+    const productId = props.navigation.getParam('productId');
+    console.log(productId)
+    const selectedProduct = useSelector(state => state.products.availableProducts).find(prod => prod.id === productId)
+
     return (
         <View>
-        <Text>The Product Detail Screen!</Text>
+            <Text>{selectedProduct.title}</Text>
         </View>
-    )};
+    )
+};
 
 const styles = StyleSheet.create({
 
