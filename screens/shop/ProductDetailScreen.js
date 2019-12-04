15e601717fa4158ae747ff242104d23d03751a62
Weston Carpenter <wc.cc96@gmail.com>
2019-12-03 19:39:50 -0800

    product detail screen navigation added

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet, Text, Image, Button, ScrollView } from 'react-native';
+
+const ProductDetailScreen = props => {
+
+    return (
+        <View>
+        <Text>The Product Detail Screen!</Text>
+        </View>
+    )};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ProductDetailScreen;
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -13,7 +13,9 @@ const ProductsOverviewScreen = props => {
                 image={itemData.item.imageUrl}
                 title={itemData.item.title}
                 price={itemData.item.price}
-                onViewDetail={() => { }}
+                onViewDetail={() => {
+                    props.navigation.navigate({ routeName: 'ProductDetail' })
+                }}
                 onAddToCart={() => { }}
             >
 
